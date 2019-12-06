4b069e023e5c1421d15182ad24faf5bf6f6d9678
tushar sharma <tsharma6@binghamton.edu>
2019-12-06 01:09:02 -0500

    code

---
+++ Stick-Hero/GameViewController.swift
@@ -0,0 +1,61 @@
+//  Testing
+
+import UIKit
+import SpriteKit
+import AVFoundation
+
+class GameViewController: UIViewController {
+    var musicPlayer:AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let scene = StickHeroGameScene(size:CGSize(width: DefinedScreenWidth, height: DefinedScreenHeight))
+        
+        
+        let skView = self.view as! SKView
+
+        skView.ignoresSiblingOrder = true
+        
+        
+        scene.scaleMode = .aspectFill
+        
+        skView.presentScene(scene)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        musicPlayer = setupAudioPlayerWithFile("bg_country", type: "mp3")
+        musicPlayer.numberOfLoops = -1
+        musicPlayer.play()
+    }
+    
+    
+    func setupAudioPlayerWithFile(_ file:NSString, type:NSString) -> AVAudioPlayer  {
+        let url = Bundle.main.url(forResource: file as String, withExtension: type as String)
+        var audioPlayer:AVAudioPlayer?
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: url!)
+        } catch {
+            print("NO AUDIO PLAYER")
+        }
+        
+        return audioPlayer!
+    }
+
+
+    override var shouldAutorotate : Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+}
